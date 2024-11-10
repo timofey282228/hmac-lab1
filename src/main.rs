577742c84ce3618f176a585a6e630e47d2e159b6
timofey282228 <tymopod-ipt25@lll.kpi.ua>
2024-11-10 02:40:19 +0200

    attack stats

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
 use rand::Rng;
 use sha2::{Digest, Sha384};
-
+use std::collections::HashMap;
 
 const INITIAL_MESSAGE: &str = "Подолянко Тимофій Олександрович";
 
@@ -9,6 +8,8 @@ const DEFAULT_ATTACK_RUN_COUNT: usize = 100;
 const PRE_ATTACK_RUN_COUNT: usize = DEFAULT_ATTACK_RUN_COUNT;
 const BD_ATTACK_RUN_COUNT: usize = DEFAULT_ATTACK_RUN_COUNT;
 
+/// quantile value for 99.5% confidence interval of mean
+const QUANTILE_0995: f64 = 2.576;
 
 fn my_hash_16(bytes: &[u8]) -> [u8; 2] {
     Sha384::digest(bytes)[Sha384::output_size() - 2..Sha384::output_size()]
@@ -41,13 +42,15 @@ fn transform_message_2(message: &str) -> String {
     let new_char = rand::thread_rng().gen_range('!'..='~');
 
     let mut new_message = String::from(message);
-    let replace_char_index = new_message.char_indices()
+    let replace_char_index = new_message
+        .char_indices()
         .nth(index)
         .expect("index less than message.chars().count()");
 
     new_message.replace_range(
         replace_char_index.0..(replace_char_index.0 + replace_char_index.1.len_utf8()),
-        &new_char.to_string());
+        &new_char.to_string(),
+    );
 
     new_message
 }
@@ -57,7 +60,7 @@ fn pre_attack<T, F, H>(
     mut transform_func: T,
     mut hash_func: F,
     incremental_transform: bool,
-)
+) -> AttackStats
 where
     T: FnMut(&str) -> String,
     F: FnMut(&[u8]) -> H,
@@ -65,6 +68,7 @@ where
 {
     let target_hash = hash_func(message.as_bytes());
     let mut other_message: String;
+    let mut attack_stats = AttackStats::default();
 
     let mut last_message = message.to_owned();
     loop {
@@ -74,45 +78,61 @@ where
             other_message = transform_func(message);
         }
 
+        attack_stats.generated_messages_count += 1;
+
         let other_hash = hash_func(other_message.as_bytes());
 
-        if target_hash == other_hash { break; }
-        if incremental_transform { last_message = other_message; }
+        if target_hash == other_hash {
+            break;
+        }
+        if incremental_transform {
+            last_message = other_message;
+        }
     }
 
     println!("Collision: {other_message} :-: {message}");
+    attack_stats
 }
 
-fn bd_attack<T, F, H>(message: &str, mut transform_func: T, mut hash_func: F)
+// TODO: incremental transform option
+fn bd_attack<T, F, H>(message: &str, mut transform_func: T, mut hash_func: F) -> AttackStats
 where
     T: FnMut(&str) -> String,
     F: FnMut(&[u8]) -> H,
     H: std::hash::Hash + Eq,
 {
     let mut map: HashMap<H, String> = HashMap::new();
+    let mut attack_stats = AttackStats::default();
 
     let initial_hash = hash_func(message.as_bytes());
     map.insert(initial_hash, message.to_owned());
 
     loop {
         let new_message = transform_func(message);
+        attack_stats.generated_messages_count += 1;
         let new_message_hash = hash_func(new_message.as_bytes());
 
         if map.contains_key(&new_message_hash) {
-            let collision = map.get_key_value(&new_message_hash)
+            let collision = map
+                .get_key_value(&new_message_hash)
                 .expect("map.contains_key(&hash) was true");
 
             if collision.1 == message {
                 // same message
-                continue
+                continue;
             }
 
-            println!("Collision: {new_message} :-: {collision}", collision=collision.1);
+            println!(
+                "Collision: {new_message} :-: {collision}",
+                collision = collision.1
+            );
             break;
         }
 
         map.insert(new_message_hash, new_message);
     }
+
+    attack_stats
 }
 
 fn prepare_run_unique_message(prefix: &str) -> String {
@@ -120,10 +140,9 @@ fn prepare_run_unique_message(prefix: &str) -> String {
     per_run_init_message.push_str(INITIAL_MESSAGE);
     per_run_init_message.push(' ');
 
-    let message_suffix: String = (0..rand::thread_rng()
-        .gen_range(0..PRE_ATTACK_RUN_COUNT / 5))
-        .map(|_| { rand::thread_rng().gen_range('!'..='~') })
-        .filter(|c| { c.is_ascii_graphic() })
+    let message_suffix: String = (0..rand::thread_rng().gen_range(0..PRE_ATTACK_RUN_COUNT / 5))
+        .map(|_| rand::thread_rng().gen_range('!'..='~'))
+        .filter(|c| c.is_ascii_graphic())
         .collect();
 
     per_run_init_message.push_str(&message_suffix);
@@ -131,11 +150,26 @@ fn prepare_run_unique_message(prefix: &str) -> String {
 }
 
 fn main() {
+    let mut cas_pre_1 = CumulativeAttackStats::default();
     for _ in 0..PRE_ATTACK_RUN_COUNT {
         let per_run_init_message = prepare_run_unique_message("PRE ");
         println!("Initial message: {per_run_init_message}");
-        pre_attack(&per_run_init_message, transform_message_1, my_hash_16, false);
+        let attack_stats = pre_attack(
+            &per_run_init_message,
+            transform_message_1,
+            my_hash_16,
+            false,
+        );
+
+        cas_pre_1.include_attack(&attack_stats);
     }
+
+    println!(
+        "Average: {avg}; variance: {var}; interval: {int:?}",
+        avg = cas_pre_1.average(),
+        var = cas_pre_1.variance(),
+        int = cas_pre_1.confidence_interval()
+    );
 
     for _ in 0..PRE_ATTACK_RUN_COUNT {
         let per_run_init_message = prepare_run_unique_message("PRE ");
@@ -153,5 +187,57 @@ fn main() {
         let per_run_init_message = prepare_run_unique_message("BD ");
         println!("Initial message: {per_run_init_message}");
         bd_attack(&per_run_init_message, transform_message_2, my_hash_32);
+    }
+}
+
+/// ?
+struct AttackStats {
+    generated_messages_count: u64,
+}
+
+struct CumulativeAttackStats {
+    sum: u64,
+    squares_sum: u64,
+    count: u64,
+}
+
+impl CumulativeAttackStats {
+    fn include_attack(&mut self, attack: &AttackStats) {
+        self.sum += attack.generated_messages_count;
+        self.squares_sum += attack.generated_messages_count.pow(2);
+        self.count += 1;
+    }
+
+    fn average(&self) -> f64 {
+        self.sum as f64 / self.count as f64
+    }
+
+    fn variance(&self) -> f64 {
+        (self.squares_sum as f64 / self.count as f64)
+            - (self.sum as f64 / self.count as f64).powi(2)
+    }
+
+    fn confidence_interval(&self) -> (f64, f64) {
+        let average = self.average();
+        let delta = (self.variance().sqrt() / (self.count as f64).sqrt()) * QUANTILE_0995;
+        (average - delta, average + delta)
+    }
+}
+
+impl Default for AttackStats {
+    fn default() -> Self {
+        Self {
+            generated_messages_count: 0,
+        }
+    }
+}
+
+impl Default for CumulativeAttackStats {
+    fn default() -> Self {
+        Self {
+            sum: 0,
+            squares_sum: 0,
+            count: 0,
+        }
     }
 }
